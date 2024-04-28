25df70c2ef63b937785b36e3eb51b0ca7b50e3a6
soumyaranjan-007 <srsahoo377@gmail.com>
2024-04-28 16:16:54 +0530

    soumya added queston 4 js file

---
+++ question4.js
@@ -0,0 +1,3 @@
+const myArray = ["Apple", "Banana", "Orange", "Mango", "Pineapple", "Grapes", "Strawberry", "Watermelon", "Peach", "Kiwi"]; 
+const slicedArray = slicedArray(myArray,2,6);
+console.log(slicedArray);
